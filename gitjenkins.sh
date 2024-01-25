3fd8750a4ee91276229e88b56f03cfef3b22f77b
Nisha Salunke <nisha.salunke2101@gmail.com>
2024-01-25 10:21:06 +0000

    1 commit message

--- gitjenkins.sh
+++ gitjenkins.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Replace these variables with your actual information
-REPO_URL="https://github.com/nishasalunke2101/Demo_INF.git"
+REPO_URL="https://nishasalunke2101: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@github.com/nishasalunke2101/Demo_INF.git"
 BRANCH="main"
 COMMIT_MESSAGE="1 commit message"
-USERNAME="nishasalunke2101"
-TOKEN="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"
 
 # Specify the path to your local repository
 REPO_PATH="/home/inferyx/git/Demo_INF"
@@ -18,16 +16,11 @@ if [ ! -d ".git" ]; then
     # Initialize the Git repository and set the remote origin
     git init
     git remote add origin "$REPO_URL"
-
-    # Set credentials for the initial push
-    git config credential.helper "store"
-    git config user.name "$USERNAME"
-    git config user.password "$TOKEN"
 fi
 
 # Git commands
 git pull origin "$BRANCH"
-echo "Hello, This is Jenkins Git! Pipeline101" > nish.txt
+echo "Hello, This is Jenkins Git! Pipeline101" > nishuu.txt
 git add .
 git commit -m "$COMMIT_MESSAGE"
 git push origin "$BRANCH"
