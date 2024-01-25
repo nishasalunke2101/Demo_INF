c441471159e90924e73808764010dbc4983aa433
Nisha Salunke <nisha.salunke2101@gmail.com>
2024-01-25 10:00:19 +0000

    1 commit message

--- gitjenkins.sh
+++ gitjenkins.sh
@@ -3,7 +3,9 @@
 # Replace these variables with your actual information
 REPO_URL="https://github.com/nishasalunke2101/Demo_INF.git"
 BRANCH="main"
-COMMIT_MESSAGE="first commit message"
+COMMIT_MESSAGE="1 commit message"
+USERNAME="nishasalunke2101"
+TOKEN="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"
 
 # Specify the path to your local repository
 REPO_PATH="/home/inferyx/git/Demo_INF"
@@ -11,16 +13,80 @@ REPO_PATH="/home/inferyx/git/Demo_INF"
 # Navigate to the local repository directory
 cd "$REPO_PATH" || exit
 
-# Initialize the Git repository and set the remote origin
-#  git init
-#  git remote add origin "$REPO_URL"
+# Check if the repository is already initialized
+if [ ! -d ".git" ]; then
+    # Initialize the Git repository and set the remote origin
+    git init
+    git remote add origin "$REPO_URL"
+
+    # Set credentials for the initial push
+    git config credential.helper "store"
+    git config user.name "$USERNAME"
+    git config user.password "$TOKEN"
+fi
 
 # Git commands
 git pull origin "$BRANCH"
-echo "Hello, This is Jenkins Git! Pipeline101" > monii.txt
+echo "Hello, This is Jenkins Git! Pipeline101" > nish.txt
 git add .
 git commit -m "$COMMIT_MESSAGE"
 git push origin "$BRANCH"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #!/bin/bash
+
+# # Replace these variables with your actual information
+# REPO_URL="https://github.com/nishasalunke2101/Demo_INF.git"
+# BRANCH="main"
+# COMMIT_MESSAGE="first commit message"
+
+# # Specify the path to your local repository
+# REPO_PATH="/home/inferyx/git/Demo_INF"
+
+# # Navigate to the local repository directory
+# cd "$REPO_PATH" || exit
+
+# # Initialize the Git repository and set the remote origin
+# #  git init
+# #  git remote add origin "$REPO_URL"
+
+# # Git commands
+# git pull origin "$BRANCH"
+# echo "Hello, This is Jenkins Git! Pipeline101" > monii.txt
+# git add .
+# git commit -m "$COMMIT_MESSAGE"
+# git push origin "$BRANCH"
 
  
 
